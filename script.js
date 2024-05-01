eaa430896a7760b941be42cc1d8158ab75daa71a
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-05-01 14:24:52 +0200

    added up to episode 49

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ document.getElementById("ep-45").onclick = function () {
     location.href = "https://pxs-yt.github.io/karasevda/episodes/45.html"
 }
 
+document.getElementById("ep-46").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/46.html"
+}
+
+document.getElementById("ep-47").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/47.html"
+}
+
+document.getElementById("ep-48").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/48.html"
+}
+
+document.getElementById("ep-49").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/49.html"
+}
