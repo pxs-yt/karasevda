7df622fa540671508d72b87319fc4a353ed0c87c
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-05-03 22:02:10 +0200

    added 4 new episodes (50-53)

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ document.getElementById("ep-48").onclick = function () {
 document.getElementById("ep-49").onclick = function () {
     location.href = "https://pxs-yt.github.io/karasevda/episodes/49.html"
 }
+
+document.getElementById("ep-50").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/50.html"
+}
+
+document.getElementById("ep-51").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/51.html"
+}
+
+document.getElementById("ep-52").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/52.html"
+}
+
+document.getElementById("ep-53").onclick = function () {
+    location.href = "https://pxs-yt.github.io/karasevda/episodes/53.html"
+}
